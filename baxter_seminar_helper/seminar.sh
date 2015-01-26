50e492a813aa8243f8e1abd4c80ad06517425715
Isaac IY Saito <130s@lateeye.net>
2015-01-26 15:59:54 +0900

    Remove setrosmaster call due to the change in upstream.

--- baxter_seminar_helper/seminar.sh
+++ baxter_seminar_helper/seminar.sh
@@ -2,25 +2,10 @@
 
 source `rospack find jsk_tools`/src/bashrc.ros
 
-# we can remove this once https://github.com/jsk-ros-pkg/jsk_common/pull/613 is merged
-type setrosmaster > /dev/null 2>&1
-if [ $? == 1 ]; then
-   function rossetmaster() { # 自分のよく使うロボットのhostnameを入れる
-       local hostname=${1-"011402P0006.local"}
-       local ros_port=${2-"11311"}
-       export ROS_MASTER_URI=http://$hostname:$ros_port
-       if [[ "${PS1}" =~ \[http://.*:.*\]\ (.*)$ ]] ; then
-           export PS1="${BASH_REMATCH[1]}"
-       fi
-       export PS1="\[\033[00;31m\][$ROS_MASTER_URI][$ROS_IP]\[\033[00m\] ${PS1}"
-       echo -e "\e[1;31mset ROS_MASTER_URI to $ROS_MASTER_URI\e[m"
-   }
-fi
-
 for (( i=1; i<=3; i++)) 
 do
   echo "#${i} ntpdate call";
-  ntpdate -q 011402P0006.local;
+  rossetmaster 011402P0006.local;
 done
 
 rossetip
