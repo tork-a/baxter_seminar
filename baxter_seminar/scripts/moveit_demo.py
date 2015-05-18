626e9188773230c46f278d2d1f5c1f632878a230
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2015-05-18 22:43:57 +0900

    Update moveit_demo.py

--- baxter_seminar/scripts/moveit_demo.py
+++ baxter_seminar/scripts/moveit_demo.py
@@ -6,7 +6,7 @@ import geometry_msgs.msg
 from moveit_commander import MoveGroupCommander
 
 if __name__ == '__main__':
-    rospy.init_node('baxter_commander', anonymous=True)
+    rospy.init_node('commander_example', anonymous=True)
     group = MoveGroupCommander("right_arm")
   
     # move to a random target
